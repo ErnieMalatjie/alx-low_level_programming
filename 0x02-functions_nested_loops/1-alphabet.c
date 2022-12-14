c6fcff14b09aeffba9c6524c3265c09d7c75e939
ErnieMalatjie <malatjiernest17@gmail.com>
2022-12-14 10:24:00 -0800

    nested loops and functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ * print_alphabet - Prints 10 times the alphabet, in lowercase.
  *
- * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
