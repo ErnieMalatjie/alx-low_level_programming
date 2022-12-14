b0a3feb03f18cf128d1c9b2f8e6d7464e5494ed2
ErnieMalatjie <malatjiernest17@gmail.com>
2022-12-14 10:14:30 -0800

    nested loop and functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,8 +4,7 @@
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  *
  * Return: Always 0 (Success)
- */
-int main(void)
+ */void print_alphabet(void)
 {
 	char letter;
 
@@ -13,5 +12,5 @@ int main(void)
 		_putchar(letter);
 
 	_putchar('\n');
-	return (0);
+	
 }
